6f08ffa44768d8a668fd4760cb609b4b5040385f
Joseph Pena <jgpena2888@gmail.com>
2020-09-16 17:00:19 -0500

    added item array

--- Game.cs
+++ Game.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace HelloWorld
 {
-    
+
     struct Item
     {
         public int statBoost;
@@ -12,12 +12,11 @@ namespace HelloWorld
 
     class Game
     {
-
         bool _gameOver = false;
         Player _player1;
         Player _player2;
-        Item sword;
-        Item dagger;
+        private Item _sword;
+        private Item _dagger;
         //Run the game
         public void Run()
         {
@@ -35,16 +34,30 @@ namespace HelloWorld
         //Performed once when the game begins
         public void Start()
         {
-            //InitializePlayers();
-            InitializeItems();
-            
+
+            //char _gameMode;
+            GetInput(out char _gameMode, "SinglePlayer(WIP)", "PvP", "Choose a gamemode");
+            switch (_gameMode)
+            {
+                case '1':
+                    Console.Clear();
+                    Console.WriteLine("test");
+                    _gameOver = true;
+                    break;
+                case '2':
+                    Console.Clear();
+                    //InitializeItems();
+                    _player1 = CreateCharacters();
+                    _player2 = CreateCharacters();
+                    break;
+            }
+
+
         }
 
         //Repeated until the game ends
         public void Update()
         {
-            _player1 = CreateCharacters();
-            _player2 = 
             StartBattle();
         }
 
@@ -54,6 +67,104 @@ namespace HelloWorld
             Console.WriteLine("press any key to exit");
             ClearScreen();
             return;
+        }
+
+        /*
+        public void InitializeItems()
+        {
+            sword.statBoost = 15;
+            dagger.statBoost = 10;
+        }
+        */
+
+        public void StartBattle()
+        {
+            Console.WriteLine("");
+            while (_player1.GetHealth() && _player2.GetHealth())
+            {
+                //player 1's turn
+                Console.WriteLine("Player 1");
+                _player1.PrintStats();
+                Console.WriteLine("");
+                Console.WriteLine("Player 2");
+                _player2.PrintStats();
+
+                //char input;
+
+                GetInput(out char input, "Attack", "nothing", "\n" + _player1.GetName() + "'s turn");
+
+                if (input == '1')
+                {
+                    _player1.Attack(_player2);
+                    if (_player2.GetHealth() == false)
+                    {
+                        ClearScreen();
+                        Console.WriteLine(_player1.GetName() + " Won!");
+                        _gameOver = true;
+                        return;
+                    }
+                }
+                else if (input == '2')
+                {
+                    Console.WriteLine(_player1.GetName() + " did nothing");
+                }
+                ClearScreen();
+
+                //player 2's turn
+                Console.WriteLine("Player 1");
+                _player1.PrintStats();
+                Console.WriteLine("");
+                Console.WriteLine("Player 2");
+                _player2.PrintStats();
+
+                GetInput(out input, "Attack", "nothing", "\n Player 2's turn");
+
+                if (input == '1')
+                {
+                    _player2.Attack(_player1);
+                    if (_player1.GetHealth() == false)
+                    {
+                        ClearScreen();
+                        Console.WriteLine(_player2.GetName() + " Won!");
+                        _gameOver = true;
+                        return;
+                    }
+                }
+                else if (input == '2')
+                {
+                    Console.WriteLine("Player 2 did nothing");
+                }
+                ClearScreen();
+            }
+        }
+
+        public Player CreateCharacters()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Player player = new Player(name, 100, 10, 5);
+            player.ChooseRole(player);
+            GiveItems(player);
+            return player;
+        }
+
+        public void GiveItems(Player player)
+        {
+
+            char input;
+            GetInput(out input, "Sword", "Dagger", "choose a weapon.");
+
+            if (input == '1')
+            {
+                player.AddItemInventory(_sword, 0);
+            }
+            else if (input == '2')
+            {
+                player.AddItemInventory(_dagger, 0);
+            }
+            Console.Clear();
+            player.PrintStats();
+            ClearScreen();
         }
 
         public void GetInput(out char input, string option1, string option2, string query)
@@ -74,104 +185,6 @@ namespace HelloWorld
                     Console.WriteLine("invaild input");
                 }
             }
-        }
-
-        public void InitializeItems()
-        {
-            sword.statBoost = 15;
-            dagger.statBoost = 10;
-        }
-
-        public void GiveItems(Player player)
-        {
-
-            char input;
-            GetInput(out input, "Sword", "Dagger", "Player 1 choose a weapon.");
-
-            if (input == '1')
-            {
-                player.EquipItem(sword);
-            }
-            else if (input == '2')
-            {
-                player.EquipItem(dagger);
-            }
-            Console.Clear();
-            Console.WriteLine("player 1");
-            player.PrintStats();
-            ClearScreen();
-            //input for player 2
-            GetInput(out input, "Sword", "Dagger", "Player 2 choose a weapon.");
-
-            if (input == '1')
-            {
-                player.EquipItem(sword);
-            }
-            else if (input == '2')
-            {
-                player.EquipItem(dagger);
-            }
-            Console.Clear();
-            Console.WriteLine("player 2");
-            player.PrintStats();
-            ClearScreen();
-        }
-
-        public void StartBattle()
-        {
-            Console.WriteLine("");
-            while (_player1.GetIsAlive() && _player2.GetIsAlive())
-            {
-
-                //player 1's turn
-                Console.WriteLine("Player 1");
-                _player1.PrintStats();
-                Console.WriteLine("");
-                Console.WriteLine("Player 2");
-                _player2.PrintStats();
-
-                char input;
-
-                GetInput(out input, "Attack", "nothing", "\n Player 1's turn");
-
-                if (input == '1')
-                {
-                    _player1.Attack(_player2);
-                }
-                else if (input == '2')
-                {
-                    Console.WriteLine("Player 1 did nothing");
-                }
-                ClearScreen();
-
-                //player 2's turn
-                Console.WriteLine("Player 1");
-                _player1.PrintStats();
-                Console.WriteLine("");
-                Console.WriteLine("Player 2");
-                _player2.PrintStats();
-
-                GetInput(out input, "Attack", "nothing", "\n Player 2's turn");
-
-                if (input == '1')
-                {
-                    _player2.Attack(_player1);
-                }
-                else if (input == '2')
-                {
-                    Console.WriteLine("Player 2 did nothing");
-                }
-                ClearScreen();
-            }
-        }
-
-        public Player CreateCharacters()
-        {
-            Console.WriteLine("What is your name?");
-            string name = Console.ReadLine();
-            player = new Player(name, 100, 10);
-            GiveItems(player);
-            return player;
         }
 
         public void ClearScreen()
--- Player.cs
+++ Player.cs
@@ -7,25 +7,35 @@ namespace HelloWorld
 {
     class Player
     {
-
+        private string _name;
+        private string _role;
         private int _health;
         private int _damage;
-        private string _name;
+        private Item[] _inventory;
 
         public Player()
         {
+            _inventory = new Item[3];
             _health = 100;
             _damage = 10;
         }
-        public void EquipItem(Item weapon)
-        {
-            _damage += weapon.statBoost;
-        }
-        public Player(string nameVal, int healthVal, int damageVal)
+
+        public Player(string nameVal, int healthVal, int damageVal, int inventorySize)
         {
             _name = nameVal;
             _health = healthVal;
             _damage = damageVal;
+            _inventory = new Item[inventorySize];
+        }
+
+
+        public void AddItemInventory (Item item, int index)
+        {
+            _inventory[index] = item;
+        }
+        public void EquipItem(int itemIndex)
+        {
+            _damage = _inventory[itemIndex].statBoost;
         }
 
         public string GetName()
@@ -33,7 +43,7 @@ namespace HelloWorld
             return _name;
         }
 
-        public bool GetIsAlive()
+        public bool GetHealth()
         {
             return _health > 0;
         }
@@ -45,17 +55,65 @@ namespace HelloWorld
 
         private void TakeDamage(int damageVal)
         {
-            if (GetIsAlive())
+            if (GetHealth())
             {
                 _health -= damageVal;
             }
-            Console.WriteLine("\n " + _name + " did " + damageVal + " damage!");
+            Console.WriteLine("\n " + _name + " took " + damageVal + " damage!");
         }
         public void PrintStats()
         {
             Console.WriteLine(_name);
+            Console.WriteLine(_role);
             Console.WriteLine("Health: " + _health);
             Console.WriteLine("Damage: " + _damage);
         }
+
+        public void ChooseRole(Player player)
+        {
+            GetInput(out char input, "Mage", "Rogue", "Knight", "Choose a role");
+
+            switch (input)
+            {
+                case '1':
+                    _role = "mage";
+                    _health = 50;
+                    _damage = 25;
+                    break;
+                case '2':
+                    _role = "Rogue";
+                    _health = 25;
+                    _damage = 50;
+                    break;
+                case '3':
+                    _role = "Knight";
+                    _health = 125;
+                    _damage = 15;
+                    break;
+            }
+            Console.Clear();
+        }
+
+        public void GetInput(out char input, string option1, string option2, string option3, string query)
+        {
+            Console.WriteLine(query);
+            Console.WriteLine("1. " + option1);
+            Console.WriteLine("2. " + option2);
+            Console.WriteLine("3. " + option3);
+            Console.Write("> ");
+
+            input = ' ';
+
+            while (input != '1' && input != '2' && input != '3')
+            {
+                input = Console.ReadKey().KeyChar;
+
+                if (input != '1' && input != '2' && input != '3')
+                {
+                    Console.WriteLine("invaild input");
+                }
+            }
+        }
+
     }
 }
